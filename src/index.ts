53a8f1a6472ba4512f80871831b402ca60d268c3
ShishirSap <shishir.sap83@gmail.com>
2024-08-14 00:22:17 +0545

    husky test

--- src/index.ts
+++ src/index.ts
@@ -1,82 +1,70 @@
-import 'reflect-metadata'
-import express, { NextFunction } from 'express'
-import { Request,Response } from 'express'
-import userRouter from './routes/users'
-import internRouter from './routes/intern'
-import rolesRouter from './routes/role'
-import { ApolloServer } from '@apollo/server'
-import {expressMiddleware} from '@apollo/server/express4'
-import cors from 'cors'
-import bodyParser from 'body-parser'
-import dotenv from 'dotenv'
-import { AppDataSource } from './data-source'
-import { interntypeDefs } from './graphql/interns/schema'
-import { internResolvers } from './graphql/interns/resolvers'
-import { usertypeDefs } from './graphql/users/schema'
-import { userResolvers } from './graphql/users/resolvers'
-import { mergeResolvers, mergeType, mergeTypeDefs } from '@graphql-tools/merge'
+import "reflect-metadata";
+import express, { NextFunction } from "express";
+import { Request, Response } from "express";
+import userRouter from "./routes/users";
+import internRouter from "./routes/intern";
+import rolesRouter from "./routes/role";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import cors from "cors";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import { AppDataSource } from "./data-source";
+import { interntypeDefs } from "./graphql/interns/schema";
+import { internResolvers } from "./graphql/interns/resolvers";
+import { usertypeDefs } from "./graphql/users/schema";
+import { userResolvers } from "./graphql/users/resolvers";
+import { mergeResolvers, mergeType, mergeTypeDefs } from "@graphql-tools/merge";
 
+const app = express();
 
-
-
-
-const app=express()
-app.use(express.urlencoded({extended:false}))
-app.use(cors())
-app.use(bodyParser.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use(bodyParser.json());
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-    console.error(err);
-    if (err instanceof Error) {
-        const sanitizedError = {
-            message: err.message,
-            stack: err.stack
-        };
-        res.status(500).json(sanitizedError);
-    } else {
-        res.status(500).json({ message: 'An unknown error occurred' });
-    }
+  console.error(err);
+  if (err instanceof Error) {
+    const sanitizedError = {
+      message: err.message,
+      stack: err.stack,
+    };
+    res.status(500).json(sanitizedError);
+  } else {
+    res.status(500).json({ message: "An unknown error occurred" });
+  }
 });
 
+dotenv.config();
+const PORT = process.env.PORT;
 
-dotenv.config()
-const PORT=process.env.PORT
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Database connection successfull");
+  })
+  .catch((error) => console.log(error));
 
+app.use("/api/users", userRouter);
+app.use("/api/intern", internRouter);
+app.use("/api/roles", rolesRouter);
 
- AppDataSource.initialize()
-.then(()=>{
-    console.log("Database connection successfull")
-
-})
-.catch((error)=>console.log(error))
-
-app.use('/api/users',userRouter)
-app.use('/api/intern',internRouter)
-app.use('/api/roles',rolesRouter)
-
-
-app.get('/',(req:Request,res:Response,next:NextFunction)=>{
-    res.send('hello world')
-})
-
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.send("hello world");
+});
 
 const server = new ApolloServer({
-  typeDefs:usertypeDefs,
-   resolvers:userResolvers
-})
-async function serverstart(){
-    await server.start()
-    app.use('/graphql',expressMiddleware(server))
+  typeDefs: usertypeDefs,
+  resolvers: userResolvers,
+});
+async function serverstart() {
+  await server.start();
+  app.use("/graphql", expressMiddleware(server));
 }
- serverstart()
+serverstart();
 
+if (process.env.NODE_ENV !== "test") {
+  app.listen(PORT, () => {
+    console.log(`Running on port ${PORT}`);
+  });
+}
 
- if(process.env.NODE_ENV !=='test'){
-    app.listen(PORT,()=>{
-        console.log(`Running on port ${PORT}`)
-    })
- }
-
-
-
-export {app,server}
-
+export { app, server };
